67323ec214edddf0f057be7761bdf47d96b8a672
Jakub <qoobaa@gmail.com>
2009-09-03 22:36:15 +0200

    changed longest-road

--- edge.rb
+++ edge.rb
@@ -1,30 +1,30 @@
 class Edge < ActiveRecord::Base
   # ...
 
-  def longest_road(visited_roads = [], skip_roads = [])
+  def longest_road(visited_roads = Set.new, skip_roads = Set.new)
     visited_roads << self
 
     # left roads
     unvisited_left_roads =
-      left_roads - visited_roads - skip_roads
+      Set.new(left_roads) - visited_roads - skip_roads
 
     left_road_lenghts = unvisited_left_roads.map do |road|
       lenght, new_visited_roads =
         road.longest_road(visited_roads, unvisited_left_roads)
 
-      visited_roads = (visited_roads + new_visited_roads).uniq
+      visited_roads = visited_roads + new_visited_roads
       lenght
     end
 
     # right roads
     unvisited_right_roads =
-      right_roads - visited_roads - skip_roads
+      Set.new(right_roads) - visited_roads - skip_roads
 
     right_road_lenghts = unvisited_right_roads.map do |road|
       lenght, new_visited_roads =
         road.longest_road(visited_roads, unvisited_right_roads)
 
-      visited_roads = (visited_roads + new_visited_roads).uniq
+      visited_roads = visited_roads + new_visited_roads
       lenght
     end
 

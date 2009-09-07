4eea83ad5c51e27aa28ca936a6b86300703fb9d6
Jakub <qoobaa@gmail.com>
2009-09-07 20:45:15 +0200

    added REST theory

---
+++ routes.rb
@@ -0,0 +1,19 @@
+ActionController::Routing::Routes.draw do |map|
+  map.resource :user
+  map.resource :user_session
+  map.resources :messages
+  map.resources :games do |games|
+    games.resource :offer
+    games.resource :offer_response
+    games.resource :player
+    games.resources :cards
+    games.resources :dice_rolls
+    games.resources :discards
+    games.resources :edges
+    games.resources :exchanges
+    games.resources :hexes
+    games.resources :nodes
+    games.resources :robberies
+  end
+  map.root :controller => "games", :action => "index"
+end
